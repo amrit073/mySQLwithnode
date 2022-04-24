dc9a5fefc6a3cfec7ce764847414633996a2c404
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-04-24 20:42:40 +0545

    Update app.js

--- app.js
+++ app.js
@@ -16,72 +16,8 @@ app.use(mycon(mysql , dbinfo , 'single'))
 app.use(express.urlencoded({ extended: false }))
 app.use('/api', api)
 
-// mysql.createConnection(dbinfo).connect(err=>console.log(err))
+
 
 app.listen(5000, ()=>(
     console.log('started at 5000')
 ))
-
-
-
-// app.get('/api:id', (req, res) => {
-//   id = req.params.id
-//   to_send = `select * from first_table where id = ${id}`
-//   con.query(to_send, (err, result) => {
-//     if (err) throw err
-//     res.send(result)
-//   })
-// }
-// )
-
-
-
-
-// app.get('/api', (req, res) => {
-//   id = req.params.id
-//   to_send = `select * from first_table `
-//   con.query(to_send, (err, result) => {
-//     if (err) throw err
-//     res.send(result)
-//   })
-// }
-// )
-
-
-// app.post('/api', (req, res) => {
-//   const { name, surname, address } = req.body
-//   console.log(req.body);
-//   to_send = `insert into first_table values (default , "${name}","${surname}", "${address}")`
-//   con.query(to_send, (err, result) => {
-//     if (err) throw err
-//     res.send(result)
-
-//   }
-//   )
-// })
-
-// app.put('/api', (req, res)=>{
-//   const { name,surname, address, id} =req.body
-// to_send= `update first_table 
-// set name = "${name}", 
-// surname = "${surname}",
-// address = "${address}"
-// where id= ${id}`
-// con.query(to_send, (err, result)=>{
-//   if (err) throw err
-//   res.send(result)
-// })
-  
-// })
-
-
-// app.delete('/api:id', (req, res) => {
-//   id = req.params.id
-//   to_send = `delete from first_table where id = ${id}`
-//   con.query(to_send, (err, result) => {
-//     if (err) throw err
-//     res.send(result)
-//   })
-// }
-// )
-
